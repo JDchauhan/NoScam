7d4247bd4ad4ea8c2ad1147d599a6e73be3cad29
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-08-07 15:38:13 +0530

    gallary implemented

--- client/assets/js/dashboard.js
+++ client/assets/js/dashboard.js
@@ -10,7 +10,15 @@ if (getCookie("token") === "") {
         },
         function (data, status, xhr) {
             console.log(data);
+            let fname = data.results.user.fname;
+            let lname = data.results.user.lname;
+            
+            fname = fname.charAt(0).toUpperCase() + fname.substr(1);
+            lname = lname.charAt(0).toUpperCase() + lname.substr(1);
+            
+            $(".username").text(fname + " " + lname);
         }).fail(function (xhr, status, error) {
         window.location.href = "../";
+        setCookie("token", "", -1);
     });
 }
