23c176753f83cb7ab543b2df9c2f8994786959d2
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-08-07 17:54:18 +0530

    frontend: get products implemented

--- client/assets/js/dashboard.js
+++ client/assets/js/dashboard.js
@@ -2,23 +2,66 @@ if (getCookie("token") === "") {
     window.location.href = "../";
 } else {
     $.ajaxSetup({
-        headers:{
-           'authorization': getCookie("token")
+        headers: {
+            'authorization': getCookie("token")
         }
     });
-    $.get("http://localhost:3000/user", {
-        },
+    $.get("http://localhost:3000/user", {},
         function (data, status, xhr) {
             console.log(data);
             let fname = data.results.user.fname;
             let lname = data.results.user.lname;
-            
+
             fname = fname.charAt(0).toUpperCase() + fname.substr(1);
             lname = lname.charAt(0).toUpperCase() + lname.substr(1);
-            
+
             $(".username").text(fname + " " + lname);
+
+            loadHome(data.results.user.role);
+
         }).fail(function (xhr, status, error) {
         window.location.href = "../";
         setCookie("token", "", -1);
     });
+}
+
+function loadHome(role) {
+    if (role === "buyer") {
+        $.get("http://localhost:3000/products", {},
+            function (data, status, xhr) {
+                console.log(data);
+                data.results.products.forEach(product => {
+                    if(!product.image){
+                        product.image = "../assets/images/test.jpg";
+                    }
+                    $('.dashboardContainer').append(
+                        '<figure class="col-md-3">' +
+                            '<a href=' + product.image + ' data-size="1600x1067">' +
+                                '<img alt="' + product.name + '" src=' + product.image + ' class="img-fluid">' +
+                            '</a>' +
+                            '<div>' +
+                                '<div>' + 
+                                    product.name +               
+                                '</div>' +
+                                    product.price +
+                                '<div>' +        
+                                    product.cc +          
+                                '</div>' +
+                                '<div>' +      
+                                    product.description +                 
+                                '</div>' +
+                                '<div>' +                       
+                                '</div>' +                            
+                            '</div>' +
+                        '</figure>'
+                    );
+                });        
+            }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+        
+
+    } else {
+
+    }
 }
