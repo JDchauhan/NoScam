0be561467069be704ec21b67b6aea99dcc1d6a52
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-08-09 16:39:35 +0530

    FrontEnd: listing products

--- client/assets/js/dashboard.js
+++ client/assets/js/dashboard.js
@@ -1,3 +1,4 @@
+var sellerID;
 if (getCookie("token") === "") {
     window.location.href = "../";
 } else {
@@ -16,7 +17,8 @@ if (getCookie("token") === "") {
             lname = lname.charAt(0).toUpperCase() + lname.substr(1);
 
             $(".username").text(fname + " " + lname);
-
+            
+            sellerID = data.results.user._id;
             loadHome(data.results.user.role);
 
         }).fail(function (xhr, status, error) {
@@ -31,106 +33,157 @@ function loadHome(role) {
             function (data, status, xhr) {
                 console.log(data);
                 data.results.products.forEach(product => {
-                    if(!product.image){
+                    if (!product.image) {
                         product.image = "../assets/images/test.jpg";
                     }
                     var description = "";
-                    if(product.description && product.description !== ""){
-                        description = '<div> Description: ' + product.description + '</div>'; 
+                    if (product.description && product.description !== "") {
+                        description = '<div> Description: ' + product.description + '</div>';
                     }
 
                     $('.buyerDashboardContainer').append(
                         '<figure class="col-md-3">' +
-                            '<a data-toggle="popover" data-img="' + product.image + '" title="' + product.name + '" data-placement="bottom" data-text="' +
-                                '<div>' + 
-                                    "Price: " + product.price +
-                                '</div>' +
-                                description +
-                            '">' +
-                                '<img alt="' + product.name + '" src=' + product.image + ' class="img-fluid">' +
-                            '</a>' +
-                            '<figcaption class="figure-caption text-right">' +
-                                '<div>' + 
-                                    product.name +               
-                                '</div>' +
-                                '<div>' + 
-                                    product.price +
-                                '</div>' +        
-                            '</figcaption>' +
+                        '<a data-toggle="popover" data-img="' + product.image + '" title="' + product.name + '" data-placement="bottom" data-text="' +
+                        '<div>' +
+                        "Price: " + product.price +
+                        '</div>' +
+                        description +
+                        '">' +
+                        '<img alt="' + product.name + '" src=' + product.image + ' class="img-fluid">' +
+                        '</a>' +
+                        '<figcaption class="figure-caption text-right">' +
+                        '<div>' +
+                        product.name +
+                        '</div>' +
+                        '<div>' +
+                        product.price +
+                        '</div>' +
+                        '</figcaption>' +
                         '</figure>'
                     );
                     $('[data-toggle="popover"]').popover({
                         html: true,
-                        content: function(){return '<img src="'+$(this).data('img') + '" height="40px" width="40px"/>' +
-                        '<p>' + $(this).data('text') + '</p>';}});
-                    });        
+                        content: function () {
+                            return '<img src="' + $(this).data('img') + '" height="40px" width="40px"/>' +
+                                '<p>' + $(this).data('text') + '</p>';
+                        }
+                    });
+                });
             }).fail(function (xhr, status, error) {
             console.log(error);
         });
-        
+
 
     } else {
-        $('.sellerDashboardContainer').append(
+        $('.addProduct').append(
             '<div class="product-form">' +
-                '<h1> Add Product </h1>' +
-                '<div id="product-add-err"></div>' +
-                '<form>' +
-                    '<div class="form-group">' +
-                        '<label for="pname">Product Name:</label>' +
-                        '<input type="text" class="form-control" id="pname" name="pname">' +
-                    '</div>' +
-                    '<div class="form-group">' +
-                        '<label for="price">Unit Price:</label>' +
-                        '<input type="number" class="form-control" id="price" name="price">' +
-                    '</div>' +
-                    '<div class="form-group">' +
-                        '<label for="description">Description:</label>' +
-                        '<input type="text" class="form-control" id="description" name="description">' +
-                    '</div>' +
-                    '<div class="form-group">' +
-                        '<label for="url">Image url:</label>' +
-                        '<input type="text" class="form-control" id="url" name="url">' +
-                    '</div>' +
-                    '<div class="form-group">' +
-                        '<label for="cc">CC:</label>' +
-                        '<input type="text" class="form-control" id="cc" name="cc">' +
-                    '</div>' +
-                    '<button type="button" class="btn btn-primary" id="product-add-btn">Submit</button>' +
-                '</form>' +
+            '<h1> Add Product </h1>' +
+            '<div id="product-add-err"></div>' +
+            '<form>' +
+            '<div class="form-group">' +
+            '<label for="pname">Product Name:</label>' +
+            '<input type="text" class="form-control" id="pname" name="pname">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="price">Unit Price:</label>' +
+            '<input type="number" class="form-control" id="price" name="price">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="description">Description:</label>' +
+            '<input type="text" class="form-control" id="description" name="description">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="url">Image url:</label>' +
+            '<input type="text" class="form-control" id="url" name="url">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="cc">CC:</label>' +
+            '<input type="text" class="form-control" id="cc" name="cc">' +
+            '</div>' +
+            '<button type="button" class="btn btn-primary" id="product-add-btn">Submit</button>' +
+            '</form>' +
             '</div>'
         );
+
+        if (sellerID) {
+            $.get("http://localhost:3000/products/seller/" + sellerID, {},
+                function (data, status, xhr) {
+                    console.log(data);
+                    data.results.products.forEach(product => {
+                        if (!product.image) {
+                            product.image = "../assets/images/test.jpg";
+                        }
+                        var description = "";
+                        if (product.description && product.description !== "") {
+                            description = '<div> Description: ' + product.description + '</div>';
+                        }
+
+                        $('.listProducts').append(
+                            '<figure class="col-md-4">' +
+                            '<a data-toggle="popover" data-img="' + product.image + '" title="' + product.name + '" data-placement="bottom" data-text="' +
+                            '<div>' +
+                            "Price: " + product.price +
+                            '</div>' +
+                            description +
+                            '">' +
+                            '<img alt="' + product.name + '" src=' + product.image + ' class="img-fluid">' +
+                            '</a>' +
+                            '<figcaption class="figure-caption text-right">' +
+                            '<div>' +
+                            product.name +
+                            '</div>' +
+                            '<div>' +
+                            product.price +
+                            '</div>' +
+                            '</figcaption>' +
+                            '</figure>'
+                        );
+                        $('[data-toggle="popover"]').popover({
+                            html: true,
+                            content: function () {
+                                return '<img src="' + $(this).data('img') + '" height="40px" width="40px"/>' +
+                                    '<p>' + $(this).data('text') + '</p>';
+                            }
+                        });
+                    });
+                }).fail(function (xhr, status, error) {
+                console.log(error);
+            });
+        }else {
+            $('.listProducts').empty();
+            $('.listProducts').append('<div class="col-md-12">Unable to fetch at this moment</div>');
+        }
     }
 }
 
 $(document).ready(function () {
-    $(document).on('click', '#product-add-btn', function(){ 
-        $.post("http://localhost:3000/products",
-        {
-            name:  $('#pname').val(),
-            price: $('#price').val(),
-            description:  $('#description').val(),
-            cc:  $('#cc').val(),
-            url:  $('#url').val(),
-        },
-        function(data, status, xhr){
-            console.log(data);
-            
-            $('#product-add-err').append(
-                '<div class="alert alert-success alert-dismissible fade show">' +
+    $(document).on('click', '#product-add-btn', function () {
+        $.post("http://localhost:3000/products", {
+                name: $('#pname').val(),
+                price: $('#price').val(),
+                description: $('#description').val(),
+                cc: $('#cc').val(),
+                url: $('#url').val(),
+            },
+            function (data, status, xhr) {
+                console.log(data);
+
+                $('#product-add-err').append(
+                    '<div class="alert alert-success alert-dismissible fade show">' +
                     '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
                     '<strong>Congratulations! </strong> Your product has been added successfully' +
-                '</div>'
-            );
-            
-        }).fail(function(xhr, status, error) {
+                    '</div>'
+                );
+
+            }).fail(function (xhr, status, error) {
             var errMsg = JSON.parse(xhr.responseText).message;
             errMsg = errMsg.charAt(0).toUpperCase() + errMsg.substr(1);
             $('#product-add-err').append(
                 '<div class="alert alert-danger alert-dismissible fade show">' +
-                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
-                    '<strong>Oops! </strong>' + errMsg +
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                '<strong>Oops! </strong>' + errMsg +
                 '</div>'
             );
         });
-    }); 
+    });
 });
