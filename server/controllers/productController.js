ed3d21c208396d0a82067932334bc4b6f117e3e6
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-08-09 15:23:04 +0530

    annonymously duplicated removal

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -55,20 +55,3 @@ module.exports.getProducts = function (req, res) {
         });
     });
 };
-
-module.exports.getProducts = function (req, res) {
-    AuthoriseUser.getUser(req, res, function (user) {
-        
-        Product.find({}, function (err, products) {
-            if (err) {
-                console.log(err);
-                return responses.errorMsg(res, 500, "Unexpected Error", "unexpected error.", null);
-            }
-
-            results = {
-                products: products
-            }
-            return responses.successMsg(res, results);
-        });
-    });
-};
