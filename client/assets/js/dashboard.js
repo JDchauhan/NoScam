55fdb273e021837610f3f16b2da7bf9d89babc8b
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-08-11 17:22:02 +0530

    add to cart implemented

--- client/assets/js/dashboard.js
+++ client/assets/js/dashboard.js
@@ -1,4 +1,4 @@
-var currentUserID;
+var currentUserID, currentUserRole;
 if (getCookie("token") === "") {
     window.location.href = "../";
 } else {
@@ -19,12 +19,13 @@ if (getCookie("token") === "") {
             $(".username").text(fname + " " + lname);
 
             currentUserID = data.results.user._id;
+            currentUserRole = data.results.user.role;
             loadHome(data.results.user.role);
 
         }).fail(function (xhr, status, error) {
-            window.location.href = "../";
-            setCookie("token", "", -1);
-        });
+        window.location.href = "../";
+        setCookie("token", "", -1);
+    });
 }
 
 function editProduct(id, name, price, description, image, cc) {
@@ -36,6 +37,41 @@ function editProduct(id, name, price, description, image, cc) {
     $('#u_cc').val(cc);
     $('.addProduct').hide();
     $('.updateProduct').show();
+}
+
+function addToCart(id){
+    $('[data-toggle=popover]').popover('hide');     
+    quantity = $('#cartItemCount').val();
+    data = {
+        "productID": id,
+        "quantity": quantity
+    };
+
+    $.ajax({
+        url: "http://localhost:3000/cart",
+        type: 'PUT',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function (result) {
+
+            $('#buyer-dashboard-err').append(
+                '<div class="alert alert-success alert-dismissible fade show">' +
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                '<strong>Congratulations! </strong> Your product has been updated successfully' +
+                '</div>'
+            );
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            var errMsg = JSON.parse(xhr.responseText).message;
+            errMsg = errMsg.charAt(0).toUpperCase() + errMsg.substr(1);
+            $('#buyer-dashboard-err').append(
+                '<div class="alert alert-danger alert-dismissible fade show">' +
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                '<strong>Oops! </strong>' + errMsg +
+                '</div>'
+            );
+        }
+    });
 }
 
 function deleteProduct(id) {
@@ -137,9 +173,9 @@ function listMyProducts(currentUserID) {
 
                 });
             }).fail(function (xhr, status, error) {
-                $('.listProducts').empty();
-                $('.listProducts').append('<h1>Oops! Some error occured</h1><div class="col-md-12">Unable to fetch your products at this moment</div>');
-            });
+            $('.listProducts').empty();
+            $('.listProducts').append('<h1>Oops! Some error occured</h1><div class="col-md-12">Unable to fetch your products at this moment</div>');
+        });
     } else {
 
         $('.listProducts').empty();
@@ -166,7 +202,7 @@ function loadHome(role) {
 
                     $('.buyerDashboardContainer').append(
                         '<figure class="col-md-3">' +
-                        '<a data-toggle="popover" data-img="' + product.image + '" title="' + product.name + '" data-placement="bottom" data-text="' +
+                        '<a data-toggle="popover" data-id="' + product._id + '" data-img="' + product.image + '" title="' + product.name + '" data-placement="bottom" data-text="' +
                         '<div>' +
                         "Price: " + product.price +
                         '</div>' +
@@ -188,13 +224,23 @@ function loadHome(role) {
                         html: true,
                         content: function () {
                             return '<img src="' + $(this).data('img') + '" height="40px" width="40px"/>' +
-                                '<p>' + $(this).data('text') + '</p>';
+                                
+                                '<div>' + $(this).data('text') + '</div>' +
+                                
+                                '<form>' +
+                                    '<br/>' +
+                                    '<div class="form-group">' +
+                                        '<input type="number" class="form-control" id="cartItemCount" value=1>' +
+                                    '</div>' +
+                                    '<button type="button" class="btn btn-default popover-btn"  onclick=addToCart("' + $(this).data('id') + '")>Add to cart</button>' +
+                                '</form>';
                         }
                     });
                 });
+
             }).fail(function (xhr, status, error) {
-                console.log(error);
-            });
+            console.log(error);
+        });
 
 
     } else {
@@ -206,19 +252,18 @@ function loadHome(role) {
 
 $(document).ready(function () {
 
-    $('.addProduct').show();
     $('.updateProduct').hide();
 
     $(document).on('click', '#product-add-btn', function () {
         window.location.href = "#";
 
         $.post("http://localhost:3000/products", {
-            name: $('#pname').val(),
-            price: $('#price').val(),
-            description: $('#description').val(),
-            cc: $('#cc').val(),
-            url: $('#url').val(),
-        },
+                name: $('#pname').val(),
+                price: $('#price').val(),
+                description: $('#description').val(),
+                cc: $('#cc').val(),
+                url: $('#url').val(),
+            },
             function (data, status, xhr) {
                 console.log(data);
 
@@ -231,15 +276,15 @@ $(document).ready(function () {
                 listMyProducts(currentUserID);
 
             }).fail(function (xhr, status, error) {
-                var errMsg = JSON.parse(xhr.responseText).message;
-                errMsg = errMsg.charAt(0).toUpperCase() + errMsg.substr(1);
-                $('#product-add-err').append(
-                    '<div class="alert alert-danger alert-dismissible fade show">' +
-                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
-                    '<strong>Oops! </strong>' + errMsg +
-                    '</div>'
-                );
-            });
+            var errMsg = JSON.parse(xhr.responseText).message;
+            errMsg = errMsg.charAt(0).toUpperCase() + errMsg.substr(1);
+            $('#product-add-err').append(
+                '<div class="alert alert-danger alert-dismissible fade show">' +
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                '<strong>Oops! </strong>' + errMsg +
+                '</div>'
+            );
+        });
     });
 
     $(document).on('click', '#product-update-btn', function () {
@@ -299,6 +344,14 @@ $(document).ready(function () {
                 $(this).popover('hide');
             }
         });
+
+        if (currentUserRole && currentUserRole === "buyer") {
+            $('.popover').off('click').on('click', function (e) {
+                e.stopPropagation(); // prevent event for bubbling up => will not get caught with document.onclick
+            });
+        }
+
+
     });
 
 });
